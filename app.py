f06384d755afb2d1f0103dbdfa38ed4f7e8a1f12
Nour Zahrawi <nourzahrawi05@gmail.com>
2026-03-15 13:43:18 +0800

    Added regions and styling to Dash app

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 import pandas as pd
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 
 df = pd.read_csv("pink_morsel_sales.csv")
 df["date"] = pd.to_datetime(df["date"])
-daily_sales = df.groupby("date", as_index=False)["sales"].sum()
-daily_sales = daily_sales.sort_values("date")
 
-fig = px.line(daily_sales, x="date", y="sales", title="Daily Sales of Pink Morsel", labels={"date": "Date", "sales": "Sales"})
 app = Dash(__name__)
 app.layout = html.Div([
     html.H1("Soul Foods Pink Morsel Sales Visualiser"), 
-    dcc.Graph(figure=fig)
-    ])
+    dcc.RadioItems(id="region-filter", 
+                   options=[{"label": "All", "value": "All"}, 
+                            {"label": "North", "value": "north"}, 
+                            {"label": "South", "value": "south"}, 
+                            {"label": "East", "value": "east"}, 
+                            {"label": "West", "value": "west"}],
+                            value="All",
+                            inline=True
+                    ),
+    dcc.Graph(id="sales-graph")
+])
+
+@app.callback(
+    Output("sales-graph", "figure"),
+    Input("region-filter", "value")
+)
+def update_graph(selected_region):
+    if selected_region == "All":
+        filtered_df = df
+    else:
+        filtered_df = df[df["region"] == selected_region]
+    
+    daily_sales = filtered_df.groupby("date", as_index=False)["sales"].sum().sort_values("date")
+    fig = px.line(daily_sales, x="date", y="sales", title="Daily Sales of Pink Morsel over Time", labels={"date": "Date", "sales": "Sales ($)"})
+    return fig
+
 if __name__ == "__main__":
     app.run(debug=True)
